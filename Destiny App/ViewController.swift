467b544f2cf2f1d8ff48d3747608a3fdf9a6a549
Hiba Abdulla <hibaabdullahkk@gmail.com>
2025-05-10 13:41:26 -0700

    Initial Commit

---
+++ Destiny App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destiny App
+//
+//  Created by Hiba Abdulla on 5/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
